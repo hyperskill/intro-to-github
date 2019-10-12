af6466a6f30ad26381fa3f4ecc0cc30f91dd86b9
Misfi <filip.misilo@gmail.com>
2019-10-12 11:29:13 +0200

    Added Scanner to main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
         System.out.println("Hello world!");
     }
