92c8398d6b7c153739bccce0cd221bbcdb9cd18e
E1StOrm <52291381+E1StOrm@users.noreply.github.com>
2024-05-13 22:31:43 +0200

    Update greeting.pl
    Added comment

--- greeting.pl
+++ greeting.pl
@@ -1,4 +1,7 @@
 #!/usr/bin/perl -w
+#########################################################################
+# © 2024 All right fucked up!                                           #
+#########################################################################
 use strict;
 
 my $name = shift || die "Run this script with one parameter, your name.";
